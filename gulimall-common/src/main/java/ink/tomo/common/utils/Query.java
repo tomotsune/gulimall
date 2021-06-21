a3889cb227ec1fb7c351151a4c5d7e9f258c4c5d
tomotsune <tomotsune@foxmail.com>
2021-06-21 09:28:10 +0800

    basic

--- gulimall-common/src/main/java/ink/tomo/common/utils/Query.java
+++ gulimall-common/src/main/java/ink/tomo/common/utils/Query.java
@@ -11,7 +11,7 @@ package ink.tomo.common.utils;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.metadata.OrderItem;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import io.renren.common.xss.SQLFilter;
+import ink.tomo.common.xss.SQLFilter;
 import org.apache.commons.lang.StringUtils;
 
 import java.util.Map;
